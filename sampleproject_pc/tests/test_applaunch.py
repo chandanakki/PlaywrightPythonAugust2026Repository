b798ae45a25e7ee17476527f38f7128bfde36a47
chandanakki <chandanakki@gmail.com>
2026-09-09 22:50:10 +0530

    first commit

---
+++ sampleproject_pc/tests/test_applaunch.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page, expect
+
+
+def testlaunch_navigate_url(page: Page):
+    page.goto("https://sgtestinginstituteapp.onrender.com/")
+
+    page.wait_for_timeout(3000)
+
+    # Fetch URL of Application
+    url = page.url
+    print("URL of the Application: " + url)
+
+    # Fetch Title of the Application
+    title = page.title()
+    print("Title of the Application: " + title)
