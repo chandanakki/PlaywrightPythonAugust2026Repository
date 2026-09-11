ac4c54ee5be9b8a85ae1d19520cd20874018a669
chandanakki <chandanakki@gmail.com>
2026-09-11 20:16:22 +0530

    Recursive Function And Exception Handling

---
+++ sampleworkspace/functions_return/namedparameter.py
@@ -0,0 +1,13 @@
+#Case 2: Named Parameters
+ 
+def display_employee(ename, jobname="Health Analyst", dname="Health", sal=45000):
+    print("Emp0loyee Name :",ename)
+    print("Emp0loyee Job Name :",jobname)
+    print("Department Name :",dname)
+    print("Employee Salary :",sal)
+    print("-------------------")
+ 
+display_employee("Santosh")
+display_employee("Santosh", jobname="Senior Doctor")
+display_employee("Adams", jobname="Senior Doctor", dname="Research")
+display_employee("Richard", jobname="Senior Analyst", dname="Research and Science")#
---
+++ sampleworkspace/functions_return/parameters.py
@@ -0,0 +1,11 @@
+#Case 1: Number of parameters available for function the same number of parameters, we should supply during the execution of the function
+ 
+# Number of parameters available for function the same number of parameters 
+# we should supply during the execution of the function
+def multiplication(p, q):
+    result=(p * q)
+    print("Multiplication Result :",result)
+ 
+multiplication(4,5)
+multiplication(10,5)
+ 
