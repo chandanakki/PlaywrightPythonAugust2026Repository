880bd7693c56674f4cbfb152fd0c8f0cf30dfb0f
chandanakki <chandanakki@gmail.com>
2026-09-23 09:17:41 +0530

    Added Pytest Unit Testing Framework

---
+++ pytestproject/sourcecode/mathsbasics.py
@@ -0,0 +1,11 @@
+def addition(num1,num2):
+    result=(num1 + num2)
+    return result
+ 
+def multiplication(num1, num2):
+    result=(num1 * num2)
+    return result
+ 
+def substraction(num1, num2):
+    result=(num1 - num2)
+    return result
